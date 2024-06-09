70e37ca0a31d302cf9d2cf86275af7266c7a4c05
surayya.urazimbetova <surayya.urazimbetova@gmail.com>
2024-06-09 11:32:35 +0200

    project files

---
+++ LogComponent/AsyncLog.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+
+namespace LogComponent
+{
+    public class AsyncLog : ILog
+    {
+        private Thread runThread;
+        private List<LogLine> lines = new List<LogLine>();
+        private StreamWriter writer; 
+        private bool exit;
+        private bool QuitWithFlush = false;
+        private DateTime curDate = DateTime.Now;
+
+
+        public AsyncLog()
+        {
+            if (!Directory.Exists(@"C:\LogTest"))
+            {
+                Directory.CreateDirectory(@"C:\LogTest");
+            }
+            this.writer = File.AppendText(@"C:\LogTest\Log" + DateTime.Now.ToString("yyyyMMdd HHmmss fff") + ".log");
+            this.writer.Write("Timestamp".PadRight(25, ' ') + "\t" + "Data".PadRight(15, ' ') + "\t" + Environment.NewLine);
+            this.writer.AutoFlush = true;
+            this.runThread = new Thread(this.MainLoop);
+            this.runThread.Start();
+        }
+
+        
+        private void MainLoop()
+        {
+            while (!this.exit)
+            {
+                if (this.lines.Count > 0)
+                {
+                    int f = 0;
+                    List<LogLine> _handled = new List<LogLine>();
+
+                    foreach (LogLine logLine in this.lines)
+                    {
+                        f++;
+
+                        if (f > 5)
+                            continue;
+                        
+                        if (!this.exit || this.QuitWithFlush)
+                        {
+                            _handled.Add(logLine);
+
+                            StringBuilder stringBuilder = new StringBuilder();
+
+                            if ((DateTime.Now - curDate).Days != 0)
+                            {
+                                doSomething(stringBuilder);
+                            }
+
+                            stringBuilder.Append(logLine.Timestamp.ToString("yyyy-MM-dd HH:mm:ss:fff"));
+                            stringBuilder.Append("\t");
+                            stringBuilder.Append(logLine.LineText());
+                            stringBuilder.Append("\t");
+
+                            stringBuilder.Append(Environment.NewLine);
+
+                            this.writer.Write(stringBuilder.ToString());
+                        }
+                    }
+
+                    for (int y = 0; y < _handled.Count; y++)
+                    {
+                        this.lines.Remove(_handled[y]);   
+                    }
+
+                    if (this.QuitWithFlush == true && this.lines.Count == 0) 
+                        this.exit = true;
+
+                    Thread.Sleep(50);
+                }
+            }
+
+            void doSomething(StringBuilder stringBuilder)
+            {
+                curDate = DateTime.Now;
+
+                this.writer = File.AppendText(@"C:\LogTest\Log" + DateTime.Now.ToString("yyyyMMdd HHmmss fff") + ".log");
+
+                this.writer.Write("Timestamp".PadRight(25, ' ') + "\t" + "Data".PadRight(15, ' ') + "\t" + Environment.NewLine);
+
+                stringBuilder.Append(Environment.NewLine);
+
+                this.writer.Write(stringBuilder.ToString());
+
+                this.writer.AutoFlush = true;
+            }
+        }
+
+        public void StopWithoutFlush()
+        {
+            this.exit = true;
+        }
+
+        public void StopWithFlush()
+        {
+            this.QuitWithFlush = true;
+        }
+
+        public void Write(string text)
+        {
+            this.lines.Add(new LogLine() { Text = text, Timestamp = DateTime.Now });
+        }
+    }
+}
---
+++ LogComponent/LogLine.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace LogComponent
+{
+    public class LogLine
+    {
+   
+        public LogLine()
+        {
+            this.Text = "";
+        }
+        public virtual string LineText()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (this.Text.Length > 0)
+            {
+                sb.Append(this.Text);
+                sb.Append(". ");
+            }
+
+            sb.Append(this.CreateLineText());
+
+            return sb.ToString();
+        }
+
+        public virtual string CreateLineText()
+        {
+            return "";
+        }
+
+
+        public string Text { get; set; }
+
+        public virtual DateTime Timestamp { get; set; }
+  
+    }
+}
